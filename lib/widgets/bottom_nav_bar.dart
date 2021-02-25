b5577c77d2f1c11a4abc6e5b39a287547ef55099
alex <alexlismile@163.com>
2021-02-25 16:01:36 +0800

    feat: 底部导航的颜色变化

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -12,21 +12,26 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  int select = 0;
   // 子组件向上传递值
   void _onItemTapped (val) {
     widget.callback(val);
+    setState(() {
+      select = val;      
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       items: [
-        BottomNavigationBarItem(icon: Icon(IconData(0xe7d5, fontFamily: 'iconfont')), title: Text('首页')),
-        BottomNavigationBarItem(icon: Icon(IconData(0xe7c0, fontFamily: 'iconfont')), title: Text('精选')),
-        BottomNavigationBarItem(icon: Icon(IconData(0xe7d6, fontFamily: 'iconfont')), title: Text('我的'))
+        BottomNavigationBarItem(icon: Icon(IconData(0xe7da, fontFamily: 'iconfont')), title: Text('首页')),
+        BottomNavigationBarItem(icon: Icon(IconData(0xe7f9, fontFamily: 'iconfont')), title: Text('精选')),
+        BottomNavigationBarItem(icon: Icon(IconData(0xe7dd, fontFamily: 'iconfont')), title: Text('我的'))
       ], // BottomNavigationBarItem 的 icon 用的是 Icon Widget实现的，这里是随便找的图标，先实现功能，后续在改成和 豆瓣电影 的一样
-      currentIndex: 0, // 默认选中的 index
-      fixedColor: Colors.black, // 选中时颜色变为黑色
+      currentIndex: select, // 默认选中的 index
+      unselectedItemColor: Color.fromARGB(255,96,98,102), // 未选中的颜色
+      fixedColor: Color.fromARGB(255,228,49,48), // 选中时颜色变为黑色
       type: BottomNavigationBarType.fixed, // 类型为 fixed
       onTap: _onItemTapped,
     );
